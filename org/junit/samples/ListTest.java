b73af1dced6c5199d6b8aed10639734764bbd0b3
egamma <egamma>
2005-05-04 08:39:40 +0000

    use new varargs syntax

--- org/junit/samples/ListTest.java
+++ org/junit/samples/ListTest.java
@@ -19,7 +19,7 @@ public class ListTest {
 	protected List<Integer> fEmpty;
 	protected List<Integer> fFull;
 
-	public static void main (String[] args) {
+	public static void main (String... args) {
 		junit.textui.TestRunner.run (suite());
 	}
 	@Before public void setUp() {
