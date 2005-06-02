a2351c724b12c3c9ff4a4872ffa90f3f0f92abb8
kbeck <kbeck>
2005-06-02 04:40:31 +0000

    *** empty log message ***

--- org/junit/tests/CommandLineTest.java
+++ org/junit/tests/CommandLineTest.java
@@ -1,7 +1,6 @@
 package org.junit.tests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
