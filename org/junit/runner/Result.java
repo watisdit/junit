e980fd7d8f6ce100022eef1631f8138c3ac9cc03
kbeck <kbeck>
2005-11-16 20:07:04 +0000

    Private listener.

--- org/junit/runner/Result.java
+++ org/junit/runner/Result.java
@@ -4,10 +4,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
-// TODO private listener
-public class Result implements RunListener {
+public class Result {
 	private int fCount= 0;
 	private int fIgnoreCount= 0;
 	private List<Failure> fFailures= new ArrayList<Failure>();
@@ -33,37 +30,38 @@ public class Result implements RunListener {
 	public int getIgnoreCount() {
 		return fIgnoreCount;
 	}
-	
+
 	public boolean wasSuccessful() {
 		return getFailureCount() == 0;
 	}
 
-	// RunListener implementation, not for external use
-	public void testRunStarted(int testCount) throws Exception {
-		fStartTime= System.currentTimeMillis();
-	}
+	private class Listener implements RunListener {
+		public void testRunStarted(int testCount) throws Exception {
+			fStartTime= System.currentTimeMillis();
+		}
 
-	public void testRunFinished(Result result) throws Exception {
-		long endTime= System.currentTimeMillis();
-		fRunTime+= endTime - fStartTime;
-	}
+		public void testRunFinished(Result result) throws Exception {
+			long endTime= System.currentTimeMillis();
+			fRunTime+= endTime - fStartTime;
+		}
 
-	public void testStarted(Object test, String name) throws Exception {
-		fCount++;
-	}
+		public void testStarted(Object test, String name) throws Exception {
+			fCount++;
+		}
 
-	public void testFinished(Object test, String name) throws Exception {
-	}
+		public void testFinished(Object test, String name) throws Exception {
+		}
 
-	public void testFailure(Failure failure) throws Exception {
-		fFailures.add(failure);
-	}
+		public void testFailure(Failure failure) throws Exception {
+			fFailures.add(failure);
+		}
 
-	public void testIgnored(Method method) throws Exception {
-		fIgnoreCount++;
+		public void testIgnored(Method method) throws Exception {
+			fIgnoreCount++;
+		}
 	}
 
 	void addListenerTo(RunNotifier notifier) {
-		notifier.addListener(this);
+		notifier.addListener(new Listener());
 	}
 }
