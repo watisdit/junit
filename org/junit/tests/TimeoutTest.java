4b2d26c2eaaaaf3509790ebd89514c2f1e7ee286
kbeck <kbeck>
2006-02-02 01:42:45 +0000

    This time with no unused imports...

--- org/junit/tests/TimeoutTest.java
+++ org/junit/tests/TimeoutTest.java
@@ -3,9 +3,6 @@ package org.junit.tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
-import java.util.concurrent.TimeoutException;
-
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.TestResult;
 import org.junit.Test;
